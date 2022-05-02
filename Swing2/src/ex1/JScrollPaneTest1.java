ebfaf3eae39115c532a15b07d776ffc147968ccd
310 <310@DESKTOP-KIJHKLK>
2022-05-02 17:00:57 +0900

    scroll

---
+++ Swing2/src/ex1/JScrollPaneTest1.java
@@ -0,0 +1,30 @@
+package ex1;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class JScrollPane1 extends JFrame{
+	
+	public JScrollPane1() {
+		Container ct = getContentPane();
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridLayout(20,5));
+		for(int i = 1; i <=100; i++) {
+			jp.add(new JButton(i + "¹ø"));
+			
+		}
+		int v = ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
+		int h = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
+		
+		JScrollPane jsp = new JScrollPane(jp,v,h);
+		ct.add(jsp, BorderLayout.CENTER);
+		setTitle("jScroll"); setSize(300,300); setVisible(true);
+	}
+}
+public class JScrollPaneTest1 {
+
+	public static void main(String[] args) {
+		new JScrollPane1();
+	}
+
+}
